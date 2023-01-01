6a9a70d41aa7b519fe6d2f6de57f1cfd096a8b56
Shomari <nyusternie@sdot.io>
2023-01-01 01:29:22 -0500

    Update statistics UI.

--- web/pages/Stats.vue
+++ web/pages/Stats.vue
@@ -1,38 +1,53 @@
 <template>
     <main class="flex justify-center">
         <!-- Page Section -->
-        <section class="max-w-5xl grid grid-cols-3 gap-5 py-10">
+        <section class="grid grid-cols-3 gap-10 py-10">
 
-            <div class="bg-pink-500 rounded-xl p-5 border-4 border-pink-700">
-                <h1 class="text-3xl font-bold text-center text-gray-200">
+            <div class="bg-rose-200 rounded-xl p-5 border-4 border-rose-400">
+                <h1 class="text-2xl font-bold text-center text-rose-700">
                     FULCRUM<small>.devops.cash</small>
                 </h1>
 
-                <div class="color-palette-set">
-                    <span class="block">#6610f2</span>
-                    <span class="block">Disabled</span>
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Status</span>
+                    <span class="text-sm font-medium text-gray-500">Disabled</span>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Uptime</span>
+                    <span class="text-sm font-medium text-gray-500">n/a</span>
                 </div>
             </div>
 
-            <div class="bg-pink-500 rounded-xl p-5 border-4 border-pink-700">
-                <h1 class="text-3xl font-bold text-center text-gray-200">
-                    BITDB<small>.devops.cash</small>
+            <div class="bg-rose-200 rounded-xl p-5 border-4 border-rose-400">
+                <h1 class="text-2xl font-bold text-center text-rose-700">
+                    ROSTRUM<small>.devops.cash</small>
                 </h1>
 
-                <div class="color-palette-set">
-                    <span class="block">#e83e8c</span>
-                    <span class="block">Synching</span>
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Status</span>
+                    <span class="text-sm font-medium text-green-700">Synching</span>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Uptime</span>
+                    <span class="text-sm font-medium text-gray-500">n/a</span>
                 </div>
             </div>
 
-            <div class="bg-pink-500 rounded-xl p-5 border-4 border-pink-700">
-                <h1 class="text-center bg-olive">
-                    SLPDB<small>.fountainhead.cash</small>
+            <div class="bg-rose-200 rounded-xl p-5 border-4 border-rose-400">
+                <h1 class="text-2xl font-bold text-center text-rose-700">
+                    BITDB<small>.fountainhead.cash</small>
                 </h1>
 
-                <div class="color-palette-set">
-                    <div class="bg-olive color-palette"><span>#3d9970</span></div>
-                    <div class="bg-olive disabled color-palette"><span>Disabled</span></div>
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Status</span>
+                    <span class="text-sm font-medium text-gray-500">Disabled</span>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <span class="text-xs uppercase">Uptime</span>
+                    <span class="text-sm font-medium text-gray-500">n/a</span>
                 </div>
             </div>
 
