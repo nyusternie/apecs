e1396229979b41ef5e1a0d4418b6780cda3bdee6
Shomari <nyusternie@sdot.io>
2023-02-01 06:48:11 -0500

    Update secure setup.

--- web/pages/Guides/Vps-Secure-Setup.vue
+++ web/pages/Guides/Vps-Secure-Setup.vue
@@ -58,6 +58,16 @@
                 <div class="my-5 w-3/4 border-4 border-yellow-300 bg-yellow-100 rounded-xl overflow-hidden">
                     <textarea rows="2" class="-mb-2 p-5 w-full border-0 bg-yellow-100">apt install -y apt-transport-https autoconf build-essential ca-certificates curl git gnupg2 libtool make net-tools software-properties-common vim</textarea>
                 </div>
+
+                <h3 class="mt-10 text-2xl font-medium">
+                    Disable Password Authentication
+                </h3>
+
+                <div class="my-5 w-3/4 border-4 border-yellow-300 bg-yellow-100 rounded-xl overflow-hidden">
+                    <textarea rows="3" class="-mb-2 p-5 w-full border-0 bg-yellow-100">vim /etc/ssh/sshd_config
+PasswordAuthentication no    &lt;-- this will be commented out by default
+sudo service ssh restart</textarea>
+                </div>
             </section>
         </template>
     </NuxtLayout>
