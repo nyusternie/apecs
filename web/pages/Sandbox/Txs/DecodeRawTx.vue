453f5b14d205f17c62c1801d37e055fff34c2379
Shomari <nyusternie@sdot.io>
2023-02-19 02:57:03 -0500

    Allow dynamic number of inputs.

--- web/pages/Sandbox/Txs/DecodeRawTx.vue
+++ web/pages/Sandbox/Txs/DecodeRawTx.vue
@@ -32,7 +32,7 @@ const reverseBytes = (_bytes) => {
 // const rawTxHex = ref(null)
 // const rawTxHex = ref('0200000001abadc7de402c7a7b84b7fb2eff5d0c8c94d661d010637a4fb5b8c11c55e6f435000000006441f8c401531747cb2cfb133a37b0817a942cbc72cbe259aec83a5d2ee8d6977a535a2050d4f2dbc53560673fbdfc17f7ef6af14e390964847ed5d4e7a487d29c094121024c750792d20e799f91cad7ebd8e67ae5e6638be213fcec8b4874a010d070db4900000000016e040000000000001976a9141d048fbba6307f595356910bca64fa0e86ca7de788ac00000000') // BCH (1 in / 1 out)
 const rawTxHex = ref('0200000002b8d95713b4a6c742f55326b2fa0bbfc7810630f7e293b9e172b67058cdcb3487010000006441bc71d8a42d1f9bf8137bcea2834770d6a1b31c5afdf7f8d4bbbb8815fcca0037b55c7fa78ae1f62e87e3ac953ab9241691ae3f8b09f4edfa402590e64d1451964121024c750792d20e799f91cad7ebd8e67ae5e6638be213fcec8b4874a010d070db490000000080d2fec073c4d7e8b539c1cd8e30a23f9b29b6d7b47e4a3b639401592194e2f50100000064413023ef0bf09b203b105a679aa337de7135c96a2361a52e28bd8981c78f4f238e5f57c995ce9982f1e1d1954f1f9d9fe90e4d5a1c8065b321e3f4bbf38d9835de4121024c750792d20e799f91cad7ebd8e67ae5e6638be213fcec8b4874a010d070db4900000000010c090000000000001976a9141d048fbba6307f595356910bca64fa0e86ca7de788ac00000000') // BCH (2 in / 1 out)
-// const rawTxHex = ref('') // BCH (1 in / 2 out)
+// const rawTxHex = ref('020000000148957a304661f6426cb13bb364d558fa6dbed2202e4cd5bcbfa208a75de1022a040000006b483045022100c4d265fc9b7b61c944a917c7efc6d378754f525448f433e64dfcb1b07c9d78c902207db08c00603dde7b07488f90f760e228f9295fcdf9afeecc34ed40cdd6e5630441210350a165309f0f0761ed321890e033bc4af5e842cf38507e9862d602e448233fdeffffffff020a1f6d00000000001976a914658fb06e86f3c3ac21c6fbf50e18dfc0747b2dbd88aceacfe300000000001976a9143cc2e4b8446d150efdc420babb77ef69422e06fb88ac00000000') // BCH_ECSDA (1 in / 2 out)
 // const rawTxHex = ref('000100c80e241c69107c397ad81730541aba81d0528d14309b2a516f6e415c484c6b9864222103cbe16ecb57d7a173ef5d46692daf38f366e2939b655817335fbfcd8a4edc41c0405e29f9533df564bc308d188e7e12a3f41954e24ba1d7a994c8d0060ea819a70c2af63d74b995f9aace3daf965df314df94b437cb662eff12861e2fd3d6974e1afeffffff39050000000000000101580200000000000017005114aa53676557ac11d41f0c66caade565cac78ca4f8605b0300') // NEXA
 
 const chainid = computed(() => {
@@ -216,8 +216,9 @@ const txAuthBlocks = computed(() => {
 
     if (chainid.value === 'BCH') {
         startPos = 82
-        endPos = 84
+        endPos = startPos + 2
         authLen = parseInt(rawTxHex.value.slice(startPos, endPos), 16) * 2
+        startPos = endPos
         endPos = startPos + authLen
 
         /* Parse authorization block. */
@@ -227,16 +228,23 @@ const txAuthBlocks = computed(() => {
             value: rawTxHex.value.slice(startPos, endPos),
         })
 
-        startPos = 364
-        endPos = 366
-        authLen = parseInt(rawTxHex.value.slice(startPos, endPos), 16) * 2
-        endPos = startPos + authLen
+        /* Parse # of remaining inputs. */
+        const numInputsRemaining = parseInt(txInputCount.value, 16) - 1
 
-        authBlocks.push({
-            startPos,
-            endPos,
-            value: rawTxHex.value.slice(startPos, endPos),
-        })
+        /* Handle remaining inputs. */
+        for (let i = 0; i < numInputsRemaining; i++) {
+            startPos = endPos + 8 + 64 + 8 // sequence + txid + vout
+            endPos = startPos + 2
+            authLen = parseInt(rawTxHex.value.slice(startPos, endPos), 16) * 2
+            startPos = endPos
+            endPos = startPos + authLen
+
+            authBlocks.push({
+                startPos,
+                endPos,
+                value: rawTxHex.value.slice(startPos, endPos),
+            })
+        }
     }
     console.log('AUTH BLOCKS', authBlocks)
 
